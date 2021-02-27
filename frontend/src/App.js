c543470d02558eaf0fce968d785b2fd4e5e4202f
Navneet Melarkode <navneet.melarkode@gmail.com>
2021-02-27 22:08:27 +0530

    lobby

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,7 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import LandingFormik from "./components/Landing";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Lobby from "./components/Lobby";
+import Container from "./components/container/Container";
 
 function App() {
 	return (
@@ -11,6 +12,7 @@ function App() {
 			<Switch>
 				<Route exact path="/" component={LandingFormik} />
 				<Route path="/lobby/:room" component={Lobby} />
+				<Route path="/board/:room" component={Container} />
 			</Switch>
 		</div>
 	);
--- frontend/src/components/Lobby.js
+++ frontend/src/components/Lobby.js
@@ -1,10 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import io from "socket.io-client";
 
 var socket = io.connect("http://localhost:5000");
 
 function Lobby(props) {
-	return <div className="container"></div>;
+	const [players, setPlayers] = useState([]);
+	return (
+		<div className="container">
+			<button type="submit" className="btn btn-primary">
+				Start
+			</button>
+			Player list:
+			<hr>{console.log(socket.on("update-lobby"))}</hr>
+		</div>
+	);
 }
 
 export default Lobby;

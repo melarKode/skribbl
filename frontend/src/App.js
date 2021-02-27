c56c128d50e207a2c5d0e134ce7f0d62801c3a3d
Navneet Melarkode <navneet.melarkode@gmail.com>
2021-02-27 19:53:24 +0530

    Basic canvas

---
+++ frontend/src/App.js
@@ -0,0 +1,9 @@
+import logo from "./logo.svg";
+import "./App.css";
+import Container from "./components/container/Container";
+
+function App() {
+	return <Container />;
+}
+
+export default App;
---
+++ frontend/src/components/container/Container.jsx
@@ -0,0 +1,63 @@
+import React from "react";
+import Board from "./../board/Board";
+
+import "./style.css";
+
+class Container extends React.Component {
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			color: "black",
+			size: "5",
+		};
+	}
+
+	changeColor(params) {
+		this.setState({
+			color: params.target.value,
+		});
+	}
+
+	changeSize(params) {
+		this.setState({
+			size: params.target.value,
+		});
+	}
+
+	render() {
+		return (
+			<div className="container">
+				<div className="tools-section">
+					<div className="color-picker">
+						Select Brush Color: &nbsp;
+						<input
+							type="color"
+							value={this.state.color}
+							onChange={this.changeColor.bind(this)}
+						/>
+					</div>
+					<div className="brush-picker">
+						Select Brush Size: &nbsp;
+						<select
+							value={this.state.size}
+							onChange={this.changeSize.bind(this)}
+						>
+							<option>5</option>
+							<option>10</option>
+							<option>15</option>
+							<option>20</option>
+							<option>25</option>
+							<option>30</option>
+						</select>
+					</div>
+				</div>
+				<div className="board-container">
+					<Board color={this.state.color} size={this.state.size} />
+				</div>
+			</div>
+		);
+	}
+}
+
+export default Container;
